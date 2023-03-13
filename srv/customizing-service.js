9d2d6325ebe20efcc02d6fc798e35cd0059cccc2
Gregor Wolf <gregor.wolf@gmail.com>
2023-03-13 22:23:40 +0100

    feat: Reflect and iterate through Entities

--- srv/customizing-service.js
+++ srv/customizing-service.js
@@ -25,28 +25,47 @@ class CustomizingService extends cds.ApplicationService {
           model: csn,
           credentials: { destination: service.destination, path: service.path },
         });
-        // TODO: Reflect Service and iterate through Entities reading the first entry
-
-        // TODO: Run only for CatalogService
-        // if() {}
-        // Read existing Orders
-        const orders = await serviceConnection.get("Orders");
-        LOG.info(`Found ${orders.length} orders`);
-        // Create a new Order
-        const order = await serviceConnection.post("Orders", {
-          OrderNo: "42",
-          CustomerOrderNo: "Order from bob",
-          currency_code: "EUR",
-          salesOrganization: "",
-          Items: [],
-          ShippingAddress: {
-            street: "Hauptstraße 1",
-            city: "Trostberg",
-          },
-        });
-        LOG.info(order);
-        const orders2 = await serviceConnection.get("Orders");
-        LOG.info(`Found ${orders2.length} orders`);
+        // Reflect Service and iterate through Entities reading the first entry
+        const m = cds.linked(csn);
+        console.log("Service", m.services);
+        console.log("Entities", m.entities);
+        for (let d of m.each("entity")) {
+          console.log(d.kind, d.name);
+          let query;
+          if (d["@odata.singleton"]) {
+            // TODO: find correct query for singleton
+          } else {
+            try {
+              query = SELECT.from(d.name).limit(1);
+              const result = await serviceConnection.run(query);
+              console.log(result);
+            } catch (error) {
+              console.log(error);
+            }
+          }
+        }
+        console.log("Operations", m.operations);
+        // Run only for CatalogService
+        if (m.services[0].name === "CatalogService") {
+          // Read existing Orders
+          const orders = await serviceConnection.get("Orders");
+          LOG.info(`Found ${orders.length} orders`);
+          // Create a new Order
+          const order = await serviceConnection.post("Orders", {
+            OrderNo: "42",
+            CustomerOrderNo: "Order from bob",
+            currency_code: "EUR",
+            salesOrganization: "",
+            Items: [],
+            ShippingAddress: {
+              street: "Hauptstraße 1",
+              city: "Trostberg",
+            },
+          });
+          LOG.info(order);
+          const orders2 = await serviceConnection.get("Orders");
+          LOG.info(`Found ${orders2.length} orders`);
+        }
       }
       return services;
     });
